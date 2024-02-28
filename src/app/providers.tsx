5a8901ebd78d292d2c257b230f6a5e9d97a01bfd
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-02-28 19:17:27 +0300

    add next ui providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NextUIPluginConfig, NextUIProvider } from "@nextui-org/react";
+
+interface Providersprops {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: Providersprops) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
