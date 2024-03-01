97471b9bdf082860ff9872e005ea7e209041cee1
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-03-01 17:23:09 +0300

    add sign in and out in the page 'use them'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@nextui-org/react";
+import * as action from "@/action";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me !!!</Button>
+      <form action={action.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={action.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
     </div>
   );
 }
